8514ed95c063cd0c2b9828be11a0480b8d90b16f
Karina Vasconcellos <karinasodre2017@gmail.com>
2026-03-30 12:19:28 -0300

    Update 02_alimentacao_banco_de_dados.sql

--- 02_alimentacao_banco_de_dados.sql
+++ 02_alimentacao_banco_de_dados.sql
@@ -1,12 +1,12 @@
 -- 1. Alimentando a base de dados com o primeiro registro --
 INSERT INTO recebimento_materias_primas
-(nome_fornecedor, numero_nota_fiscal, data_chegada, peso_total_kg, lote)
+(nome_fornecedor, numero_nota_fiscal, data_recebimento, peso_total_kg, lote)
 VALUES
 ('Fornecedor Alpha', '100530', '2026-03-29', '50.000', 'B5693A')
 
 -- 2. Alimentando a base com múltiplas linhas --
 INSERT INTO recebimento_materias_primas
-(nome_fornecedor, numero_nota_fiscal, data_chegada, peso_total_kg, lote)
+(nome_fornecedor, numero_nota_fiscal, data_recebimento, peso_total_kg, lote)
 VALUES
 ('Química Central', '10349', '2026-02-29', '50', '5649'),
 ('Essências LTDA', '10125', '2026-02-15', '400', 'ASK83'),

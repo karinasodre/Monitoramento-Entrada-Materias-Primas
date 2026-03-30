701cd60067e4c85a7a8db6ab8f57d7b8d9a54746
Karina Vasconcellos <karinasodre2017@gmail.com>
2026-03-30 00:10:06 -0300

    Insert multiple records and update weight in SQL
    Inserted multiple records into recebimento_materias_primas and corrected weight for ID 2.

---
+++ 02_alimentacao_banco_de_dados.sql
@@ -0,0 +1,19 @@
+-- 1. Alimentando a base de dados com o primeiro registro --
+INSERT INTO recebimento_materias_primas
+(nome_fornecedor, numero_nota_fiscal, data_chegada, peso_total_kg, lote)
+VALUES
+('Fornecedor Alpha', '100530', '2026-03-29', '50.000', 'B5693A')
+
+-- 2. Alimentando a base com múltiplas linhas --
+INSERT INTO recebimento_materias_primas
+(nome_fornecedor, numero_nota_fiscal, data_chegada, peso_total_kg, lote)
+VALUES
+('Química Central', '10349', '2026-02-29', '50', '5649'),
+('Essências LTDA', '10125', '2026-02-15', '400', 'ASK83'),
+('Industria Corporativa', '124', '2026-01-02', '20.000', 'SK830'),
+('Manufacturing X', '9727', '2026-02-18', '33.000', 'SJA2113');
+
+-- 3. Corrigindo o peso do ID 2 --
+UPDATE recebimento_materias_primas
+SET peso_total_kg = 50.000
+WHERE id_recebimento = 2;
